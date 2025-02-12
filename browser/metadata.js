25d008c08e7e192d0ccfacd0d01dd73e245ace7b
Will Ondrik <wondrik@my.bcit.ca>
2025-02-11 17:33:57 -0800

    Captures user metadata.

--- browser/metadata.js
+++ browser/metadata.js
@@ -10,17 +10,53 @@ class MetaData {
         this.geolocation = navigator.geolocation || null;
         this.language = navigator.language;
         this.timestamp = new Date().toISOString();
-        this.browser = getBrowser(navigator);
-        this.os = getOS(navigator);
+        this.browser = this.getBrowser(navigator);
+        this.os = this.getOS(navigator);
 
     }
 
     getBrowser(navigator){
+        if (navigator.brave) {
+            return 'Brave';
+        }
+
+        const browser = navigator.userAgent;
+        switch (browser) {
+            case browser.indexOf('Chrome') > -1:
+                return 'Chrome';
+            case browser.indexOf('Firefox') > -1:
+                return 'Firefox';
+            case browser.indexOf('Safari') > -1:
+                return 'Safari';
+            case browser.indexOf('Edge') > -1:
+                return 'Edge';
+            case browser.indexOf('MSIE') > -1:
+                return 'Internet Explorer';
+            default:
+                return 'Unknown';
+        }
 
     }
 
     getOS(navigator) {
-
+        const userAgent = navigator.userAgent;
+    
+        const osPatterns = [
+            { name: "Windows", regex: /Windows NT/i },
+            { name: "MacOS", regex: /Mac OS X/i },
+            { name: "Linux", regex: /Linux/i },
+            { name: "Android", regex: /Android/i },
+            { name: "iOS", regex: /(iPhone|iPad|iPod)/i }
+        ];
+    
+        for (const os of osPatterns) {
+            if (os.regex.test(userAgent)) {
+                console.log('os.name', os.name);
+                return os.name;
+            }
+        }
+    
+        return "Unknown OS";
     }
 
 }
