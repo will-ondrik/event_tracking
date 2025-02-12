1d03c6fdbbd9ca4b44dd4b65284cdbb7d8de9788
Will Ondrik <wondrik@my.bcit.ca>
2025-02-11 17:35:16 -0800

    Captures click events.

--- events/click.js
+++ events/click.js
@@ -6,20 +6,31 @@
  */
 class ClickEvent {
     constructor(event) {
+        console.log('ClickEvent', event);
         const target = event.target;
 
-        this.className = target.classList?.length
-            ? Array.from(target.classList.join(" "))
-            : target.className || null;
+        this.className = this.getClass(target);
         this.tagName = target.tagName;
         this.id = target.id || null;
+        this.sessionId = sessionStorage.getItem('sessionId') || null;
         this.text = target.innerText || null;
-        this.timestamp = new Date.toISOString();
+        this.timestamp = new Date().toISOString();
         this.url = window.location.href;
         this.coordinates = {
             xCoord: event.pageX,
             yCoord: event.pageY,
         };
     }
+
+    getClass(target) {
+        if (!(target instanceof HTMLElement) || !target.classList) {
+            this.className = null;
+            return;
+        }
+
+        this.className = target.classList?.length ? Array.from(target.classList.join(" ")) : target.className || null;
+        console.log('this.className', this.className);
+    }
 }
 
+export default ClickEvent;
